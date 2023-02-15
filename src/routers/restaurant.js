b6aceb55cd34201578bb7bafebeb2320a660401c
carlosarenasf <carenas@bloobirds.com>
2023-02-15 20:23:57 +0100

    permissions

--- src/routers/restaurant.js
+++ src/routers/restaurant.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const RestaurantController = require("../controllers/restaurant");
-const { adminPermissionMiddleware } = require("../security/permisions");
+const { adminPermissionMiddleware, ownPermissionMiddleware } = require("../security/permisions");
 
 const router = express.Router();
 
 router.get("/", RestaurantController.getAll);
 router.get("/:id", RestaurantController.getById);
 router.post("/", adminPermissionMiddleware, RestaurantController.create);
-router.patch("/:id", adminPermissionMiddleware, RestaurantController.edit);
+router.patch("/:id", ownPermissionMiddleware, RestaurantController.edit);
 
 module.exports = router;
--- src/security/permisions.js
+++ src/security/permisions.js
@@ -1,24 +1,46 @@
 const User = require('../model/User');
+const Restaurant = require('../model/Restaurant');
 
-const adminPermissionMiddleware = (req, res, next) => {
-    const userRole = req.jwtPayload.role; //guardado en el jwt middleware
-    if (userRole !== 'ADMIN') {
+const adminPermissionMiddleware = async (req, res, next) => {
+    const userId = req.jwtPayload._id; //guardado en el jwt middleware
+    const user = await User.findById(userId);
+    if (user.role !== 'ADMIN') {
         return res.status(401).json({error: "Unauthorized"});
     }
 
     next();
 };
 
-const globalAdminPermissionMiddleware = (req, res, next) => {
-    const userRole = req.jwtPayload.role; //guardado en el jwt middleware
-    if (userRole !== 'GLOBAL_ADMIN') {
+const globalAdminPermissionMiddleware = async (req, res, next) => {
+    const userId = req.jwtPayload._id; //guardado en el jwt middleware
+    const user = await User.findById(userId);
+    if (user.role !== 'GLOBAL_ADMIN') {
         return res.status(401).json({error: "Unauthorized"});
     }
 
     next();
+};
+
+const ownPermissionMiddleware = async (req, res, next) => {
+    const userId = req.jwtPayload._id; //guardado en el jwt middleware
+    const user = await User.findById(userId);
+
+    if (user.role === 'GLOBAL_ADMIN' || user.role === 'ADMIN') {
+        return next();
+    }
+
+    const restaurant = await Restaurant.findById(req.params.id);
+
+    if (restaurant.owner !== userId) {
+        return res.status(401).json({error: "Unauthorized"});
+    }
+
+    next();
+
 };
 
 module.exports = {
     adminPermissionMiddleware,
-    globalAdminPermissionMiddleware
+    globalAdminPermissionMiddleware,
+    ownPermissionMiddleware
 }
